43f89fee94d89fed6ea6cf1ddb9acc3b93a3d1ca
Webwiz <aliuanas004@gmail.com>
2024-07-27 23:33:33 +0100

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import { Navbar } from "./Components/Navbar"
 function App() {
 
   return (
-    <div className="bg-black font-sans h-100vh flex">
+    <div className="bg-black font-sans h-100vh">
       <Navbar />
       
       <Routes>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,8 +7,8 @@ import contact from '../assets/images/contact.png'
 export const Navbar = () => {
   return (
     <>
-      <nav className='flex md:justify-center border-r-2 border-yellow-500 text-yellow-500'>
-        <ul className="flex flex-col p-1 gap-3 align-middle justify-between md:justify-center md:gap-20">
+      <nav className='px-3 flex md:justify-center border-b-2 border-yellow-500 text-yellow-500'>
+        <ul className="flex flex-1 p-3 gap-2 align-middle justify-between md:justify-center md:gap-20">
           <li className='flex gap-1 md:gap-2 justify-center items-center text-sm font-mono font-semibold hover:bg-yellow-400 hover:rounded-3xl hover:text-black p-2 '>
             <img src={home} alt="" className='w-3 h-3 fill-yellow-500' />
             <Link to="/" className=''>Home</Link>
